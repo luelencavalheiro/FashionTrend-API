e99ba81fb043232f0483c57c6577168887c4a422
Luelen Cavalheiro <luelencavalheiro@hotmail.com>
2023-11-08 19:36:59 -0300

    estruturação do projeto

---
+++ FashionTrend.Domain/Entities/Payment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using FashionTrend.Domain.Enums;
+
+namespace FashionTrend.Domain.Entities;
+
+public class Payment : BaseEntity
+{
+    public decimal Amount { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+    public DateTimeOffset PaymentDate { get; set; }
+    public Guid? RequestId { get; set; }
+
+    [JsonIgnore]
+    public virtual Request Request { get; set; }
+}
+
